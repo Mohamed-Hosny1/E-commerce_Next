de949350ac1de174b2c343ac98ed4fd6d6b22515
Mohamed-Hosny1 <hosny0169@gmail.com>
2026-04-29 06:19:27 +0300

    edit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,30 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const isSecure = process.env.NODE_ENV === "production";
+  const cookieName = isSecure
+    ? "__Secure-authjs.session-token"
+    : "authjs.session-token";
+
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET!,
-    salt: "authjs.session-token",
+    salt: cookieName,
   });
 
   const { pathname } = request.nextUrl;
 
-  const authpages = pathname == "/login" || pathname == "/register";
+  const authPages = pathname === "/login" || pathname === "/register";
 
-  if (token && authpages) {
+  if (token && authPages) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  if (!token && !authpages) {
+  if (!token && !authPages) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/cart", "/allorders", "/brands", "/wishlist", "/login", "/register"]
+  matcher: ["/cart", "/allorders", "/brands", "/wishlist", "/login", "/register"],
 };
